856a90c4b07c728b5ba73e48174338150a4ff562
Codey Pinero <codey.pinero@gmail.com>
2019-03-29 02:13:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,6 +20,9 @@ def apply_coupons(cart, coupons)
       else
         cart["#{name} W/ COUPON"] = {:count => 1, :price => coupon[:cost]}
         cart["#{name} W/ COUPON"][:clearance] = cart[name][:clearance]
+      end
+      cart[name][:count] -= coupon[:num]
+    end
   end
 end
 
