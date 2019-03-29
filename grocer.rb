4fd8400218f158260bf8c6747f03ead27c042cea
Codey Pinero <codey.pinero@gmail.com>
2019-03-29 02:21:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,4 +41,5 @@ def checkout(cart, coupons)
   consolidated_cart = consolidated_cart(cart)
   coupon_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(coupon_cart)
+  total = 0
 end
