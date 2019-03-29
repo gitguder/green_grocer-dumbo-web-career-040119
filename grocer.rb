2b00874f18057d61195b5da3a6bc152a0dfa19c2
Codey Pinero <codey.pinero@gmail.com>
2019-03-29 02:03:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,8 @@
 def consolidate_cart(cart)
   cart.each_with_object({}) do |item, output|
-    item.each { |type, attribute| output[type] ? attribute[:count] += 1 : attribute[:count] = 1}
+    item.each do |type, attribute| 
+      if output[type] 
+        attribute[:count] += 1 : attribute[:count] = 1
 end
 
 def apply_coupons(cart, coupons)
